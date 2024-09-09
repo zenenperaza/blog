0efeebdf34266b898ac8a09f9442b41b69a96f4c
Zenen Peraza <peraza@outlook.com>
2024-09-09 16:04:37 -0400

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hola desde inicio';
+});
+
+
+Route::get('/contacto', function () {
+    return 'hola desde Contacto';
 });
