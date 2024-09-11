9850fcb7d95c0eed7de0257e0516d58efbddd7af
Zenen Peraza <peraza@outlook.com>
2024-09-11 14:01:20 -0400

    grupo de rutas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    // public function index(){
+    //     return "Hola desde la pagina Home";
+    // }
+
+    public function __invoke(){
         return "Hola desde la pagina Home";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,24 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', HomeController::class);
 
-Route::get('/post', [PostController::class, 'index'])->name('post.index');
+Route::controller(PostController::class)->group(function(){
 
-Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+    Route::get('/posts', 'index')->name('post.index');
 
-Route::post('/post', [PostController::class, 'store'])->name('post.store');
+    Route::get('/posts/create',  'create')->name('post.create');
 
-Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
+    Route::post('/posts', 'store')->name('post.store');
 
-Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+    Route::get('/posts/{post}', 'show')->name('post.show');
 
-Route::put('/post/{post}', [PostController::class, 'update'])->name('post.update');
+    Route::get('/posts/{post}/edit', 'edit')->name('post.edit');
 
-Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+    Route::put('/posts/{post}', 'update')->name('post.update');
 
+    Route::delete('/posts/{post}', 'destroy')->name('post.destroy');
 
+});
+
+// Route::resource('posts', PostController::class);
